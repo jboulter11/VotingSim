5ab2a96d05646ea348ea6ddf8a47839dfc539a08
Jim Boulter <jboulter11@gmail.com>
2020-10-30 21:23:58 -0700

    Initial Commit

---
+++ VotingSim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VotingSim
+//
+//  Created by James Boulter on 10/30/20.
+//  Copyright © 2020 James Boulter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VotingSimTests/VotingSimTests.swift
@@ -0,0 +1,34 @@
+//
+//  VotingSimTests.swift
+//  VotingSimTests
+//
+//  Created by James Boulter on 10/30/20.
+//  Copyright © 2020 James Boulter. All rights reserved.
+//
+
+import XCTest
+@testable import VotingSim
+
+class VotingSimTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
